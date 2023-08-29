2600e1ca15e722989539cab0acaf6ab92916e373
Priscila Igarashi <132402519+prrrri@users.noreply.github.com>
2023-08-29 20:07:40 -0300

    hello world

---
+++ helloworld/src/main/java/com/generation/helloworld/controller/ControllerTeste.java
@@ -0,0 +1,41 @@
+package com.generation.helloworld.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello-world")
+public class ControllerTeste {
+	@GetMapping
+	public String helloWorld() {
+		return "Hello World!!!";
+	}
+	
+    @GetMapping("/bsm")
+    public String ListaBSM() {
+        return "<pre>\n" +
+        		"Lista de BSM da Generation\n" +
+                "Mentalidade de Crescimento\n" +
+                "Persistência\n" +
+                "Responsabilidade Pessoal\n" +
+                "Orientação ao Futuro\n" +
+                "Comunicação\n" +
+                "Orientação ao Detalhe\n" +
+                "Proatividade\n" +
+                "Trabalho em Equipe\n" +
+                "</pre>";
+	}
+    
+    @GetMapping("/objetivos")
+    public String ListaObjetivos() {
+        return "<pre>\n" +
+                "Meus Objetivos de Aprendizagem da Semana\n" +
+                "Revisar conteúdo de MySQL\n" +
+                "Assistir vídeos da Plataforma\n" +
+                "Absorver conteúdo de Spring\n" +
+                "</pre>";
+	}
+	
+		
+	}
